2e1beb1dd494cc57904d2bf746cb60937f827246
razor999920 <43019476+razor999920@users.noreply.github.com>
2020-12-28 15:21:40 -0500

    dojo locator

--- Dojo/src/main/java/com/dojo/Dojo/Model/Dojo.java
+++ Dojo/src/main/java/com/dojo/Dojo/Model/Dojo.java
@@ -1,5 +1,68 @@
 package com.dojo.Dojo.Model;
 
 public class Dojo {
-    
+    private String name;
+    private String province;
+    private String city;
+    private String address;
+    private String ChiefInstruc;
+    private String Phone;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getChiefInstruc() {
+        return ChiefInstruc;
+    }
+
+    public void setChiefInstruc(String chiefInstruc) {
+        ChiefInstruc = chiefInstruc;
+    }
+
+    public String getPhone() {
+        return Phone;
+    }
+
+    public void setPhone(String phone) {
+        Phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
 }
